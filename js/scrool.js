227321864691bc3bbcc38ef5edbab8d8d683939e
Pedro Figueiredo <pedrohfm2012@yahoo.com.br>
2021-08-10 12:19:14 -0300

    Layout Topografia

---
+++ js/scrool.js
@@ -0,0 +1,22 @@
+window.sr = ScrollReveal({ reset: true });
+
+// Custom Settings
+sr.reveal('.foo-1', { duration: 200 });
+
+sr.reveal('.foo-2', { 
+  origin: 'right', 
+  duration: 3000 
+});
+
+sr.reveal('.foo-3', { 
+  viewFactor: 0.2,
+  duration: 2000
+});
+
+sr.reveal('.foo-4', { 
+  viewFactor: 0.2
+});
+
+sr.reveal('.foo-5', { 
+  duration: 200 
+});
